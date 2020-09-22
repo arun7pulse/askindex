c57bd7f0f235266a41925896839f07407da463b4
Arunsanthoshkumar Annamalai <arun7pulse@gmail.com>
2020-09-22 18:03:53 +0530

    Week/Month Commit

--- app.py
+++ app.py
@@ -25,12 +25,38 @@ def indices():
     except Exception:
         return traceback.format_exc()
 
+@app.route('/indicesweek',methods=["GET"])
+@cross_origin(origin='*' ,headers=['Content- Type'])
+def indicesweek():
+    try:
+        symbol = request.args.get('symbol')
+        nf = Indices(symbol=symbol)
+        return nf.dfw.tail(15).to_html()
+    except Exception:
+        return traceback.format_exc()
+
+@app.route('/indicesmonth',methods=["GET"])
+@cross_origin(origin='*' ,headers=['Content- Type'])
+def indicesmonth():
+    try:
+        symbol = request.args.get('symbol')
+        nf = Indices(symbol=symbol)
+        return nf.dfwm.tail(15).to_html()
+    except Exception:
+        return traceback.format_exc()
+
+
 @app.route("/")
 def main():
     return """<h2>Indices</h2>
               <ul>
               <li><a href="/indices?symbol=NIFTY%2050"> NIFTY 50</a> </li>
+              <li><a href="/indicesweek?symbol=NIFTY%2050"> NIFTY 50 Weekly (Thu)</a> </li>
+              <li><a href="/indicesmonth?symbol=NIFTY%2050"> NIFTY 50 Month </a> </li>
+              <li> ------------------------------------------------------------</li>
               <li><a href="/indices?symbol=NIFTY%20BANK"> NIFTY BANK</a> </li>
+              <li><a href="/indicesweek?symbol=NIFTY%20BANK"> NIFTY BANK Weekly(Thu) </a> </li>
+              <li><a href="/indicesmonth?symbol=NIFTY%20BANK"> NIFTY BANK Month </a> </li>
               </ul>"""
 
 if __name__ == "__main__":
